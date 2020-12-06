22b9b0015c8adaa509be5514c66f17f31317fa23
Georg Grebenyuk <55656376+GeorgGrebenyuk@users.noreply.github.com>
2020-12-07 01:10:09 +0300

    Create project

---
+++ Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿
+namespace PointCloudToGLB
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.label2 = new System.Windows.Forms.Label();
+			this.button2 = new System.Windows.Forms.Button();
+			this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+			this.button3 = new System.Windows.Forms.Button();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.listBox1 = new System.Windows.Forms.ListBox();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(13, 13);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(175, 13);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Укажите исходное облако точек:";
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(194, 8);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "Обзор";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.FileName = "openFileDialog1";
+			this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(12, 48);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(170, 13);
+			this.label2.TabIndex = 0;
+			this.label2.Text = "Укажите папку для сохранения:";
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(194, 43);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 2;
+			this.button2.Text = "Обзор";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// folderBrowserDialog1
+			// 
+			this.folderBrowserDialog1.HelpRequest += new System.EventHandler(this.folderBrowserDialog1_HelpRequest);
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(194, 154);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(75, 23);
+			this.button3.TabIndex = 3;
+			this.button3.Text = "Запуск";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(12, 159);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(94, 13);
+			this.label3.TabIndex = 0;
+			this.label3.Text = "Запуск процесса";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(12, 79);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(152, 13);
+			this.label4.TabIndex = 0;
+			this.label4.Text = "Выберите формат экспорта:";
+			// 
+			// listBox1
+			// 
+			this.listBox1.FormattingEnabled = true;
+			this.listBox1.Items.AddRange(new object[] {
+            "GLTF",
+            "GLB",
+            "OBJ"});
+			this.listBox1.Location = new System.Drawing.Point(16, 96);
+			this.listBox1.Name = "listBox1";
+			this.listBox1.Size = new System.Drawing.Size(148, 56);
+			this.listBox1.TabIndex = 4;
+			this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.ClientSize = new System.Drawing.Size(281, 189);
+			this.Controls.Add(this.listBox1);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Name = "Form1";
+			this.Text = "PointCloud to Chronos";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.OpenFileDialog openFileDialog1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.ListBox listBox1;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Numerics;
+using SharpGLTF.IO;
+using SharpGLTF.Geometry;
+using SharpGLTF.Geometry.VertexTypes;
+using SharpGLTF.Materials;
+using SharpGLTF.Schema2;
+
+namespace PointCloudToGLB
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+		StringBuilder sb = new StringBuilder();
+		int i1 = 1;
+
+		private void openFileDialog1_FileOk(object sender, CancelEventArgs e) //Операция открытия облака точек исходного
+		{
+			
+		}
+
+		private void button1_Click(object sender, EventArgs e) //Кнопка открыть исходное облако точек
+		{
+			if (openFileDialog1.ShowDialog() == DialogResult.Cancel) return;
+		}
+
+		private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e) //Каталог для сохранения
+		{
+			
+		}
+
+		public void Operations(StringBuilder sb, int i1, string SaveFolderPath)
+		{
+			var mesh = VertexBuilder<VertexPosition, VertexColor1, VertexEmpty>.CreateCompatibleMesh("points");
+			var material = new MaterialBuilder();
+			var model = ModelRoot.CreateModel();
+			var guid = Guid.NewGuid();
+
+			string DataStr = null;
+			using (var reader = new StringReader(sb.ToString()))
+			{
+				while ((DataStr = reader.ReadLine()) != null)
+				{
+					string[] SingleStrValues = DataStr.Split(' ');
+					float R_code = (float)Convert.ToDouble(SingleStrValues[3]);
+					float G_code = (float)Convert.ToDouble(SingleStrValues[4]);
+					float B_code = (float)Convert.ToDouble(SingleStrValues[5]);
+					System.Numerics.Vector3 VectorOfPoint = new System.Numerics.Vector3((float)Convert.ToDouble(SingleStrValues[0]), (float)Convert.ToDouble(SingleStrValues[2]), (float)Convert.ToDouble(SingleStrValues[1]));
+
+					mesh.UsePrimitive(material, 1).AddPoint((VectorOfPoint, new Vector4(R_code / 256f, G_code / 256f, B_code / 256f, 1)));
+
+				}
+			}
+			model.CreateMeshes(mesh);
+			model.UseScene("Default").CreateNode().WithMesh(model.LogicalMeshes[0]);
+			if (listBox1.SelectedItem.ToString() == "GLTF") { model.SaveGLTF($@"{SaveFolderPath}\Export_Part-{i1}.gltf"); }
+			else if (listBox1.SelectedItem.ToString() == "GLB") { model.SaveGLB($@"{SaveFolderPath}\Export_Part-{i1}.glb"); }
+			else if (listBox1.SelectedItem.ToString() == "OBJ") { model.SaveAsWavefront($@"{SaveFolderPath}\Export_Part-{i1}.obj"); }
+			mesh = null;
+
+		}
+
+		private void button2_Click(object sender, EventArgs e) //Кнопка выбора папки сохранения
+		{
+			if (folderBrowserDialog1.ShowDialog() == DialogResult.Cancel) return;
+		}
+		string SaveFolderPath = null;
+		private void button3_Click(object sender, EventArgs e) //Кнопка запуска процесса
+		{
+			var guid = Guid.NewGuid();
+			SaveFolderPath = folderBrowserDialog1.SelectedPath + @"\" + guid;
+			System.IO.Directory.CreateDirectory(SaveFolderPath);
+			
+
+			//string FilePath = @"C:\Users\GeorgKeneberg\Documents\Temp\Region02.pts";
+			long Counter = 0;
+
+			foreach (var SingleStr in File.ReadLines(openFileDialog1.FileName))
+			{
+				sb.AppendLine(SingleStr);
+				Counter++;
+
+				if (Counter == 2000000)
+				{
+					Operations(sb, i1, SaveFolderPath);
+					i1++;
+					sb.Clear();
+					Counter = 0;
+				}
+
+			}
+			Operations(sb, i1, SaveFolderPath);
+		}
+
+		private void listBox1_SelectedIndexChanged(object sender, EventArgs e) //Выбор типа экспортируемого файла
+		{
+
+		}
+	}
+}
